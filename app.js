1bc2f8a8bfac89939e6db488cb814b934aa797ff
Suresh <suresh.avlm@gmail.com>
2017-05-14 10:04:02 +0530

    initial committ

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 
-var port =  process.env.PORT | 8080;
+var port =  process.env.PORT || 8080;
 
 app.listen(port, function() {
 	console.log('Server is running at http://localhost:'+ port);
