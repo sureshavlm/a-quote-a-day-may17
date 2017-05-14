7078db1fe740c06faa8478ebdc794dea55ffc48b
Suresh <suresh.avlm@gmail.com>
2017-05-14 09:53:36 +0530

    initial committ

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+var port =  process.env.PORT | 8080;
+
+app.listen(port, function() {
+	console.log('Server is running at http://localhost:'+ port);
+});
+
